8ab8fd58897cae5ccb82c82a2bb6ecb0ed8fc166
Hamsuke <43707720+Hamsuke@users.noreply.github.com>
2024-04-27 15:18:25 -0600

    Update AppConfig.cs

--- Barrio1/Models/AppConfig.cs
+++ Barrio1/Models/AppConfig.cs
@@ -4,7 +4,7 @@ namespace Barrio1.Models
 {
     internal class AppConfig
     {
-        public const string supaUrl = "https://pnovjbpbnsmkerehrvsd.supabase.co";
-        public const string supaKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBub3ZqYnBibnNta2VyZWhydnNkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMTc3NTcxNCwiZXhwIjoyMDI3MzUxNzE0fQ.oxBV1LfSqRZ0YWwN1f3qaQXCACI9Q6E200RoyLuiTpY";
+        public const string supaUrl = "";
+        public const string supaKey = "";
     }
 }
